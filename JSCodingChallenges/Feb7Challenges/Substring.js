ac4b89149ba472a613f52da7b468a043aec602fe
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2022-02-10 09:40:04 -0500

    added test cases and removed unnecessary condition

--- JSCodingChallenges/Feb7Challenges/Substring.js
+++ JSCodingChallenges/Feb7Challenges/Substring.js
@@ -29,7 +29,7 @@ const getVowelSubstrings = str => {
   for (i = 0; i < str.length; i++){
     for(j = 1; j <= str.length; j++){
       let sub = str.substring(i, j);
-      if (vowels.includes(sub.charAt(0)) && vowels.includes(sub.charAt(sub.length-1)) && sub != '' && !subArray.includes(sub)){
+      if (vowels.includes(sub.charAt(0)) && vowels.includes(sub.charAt(sub.length-1)) && !subArray.includes(sub)){
         subArray.push(sub);
       }
     }
@@ -53,4 +53,6 @@ const getConsonantSubstrings = str => {
 }
 console.log(getVowelSubstrings('apple'));
 console.log(getVowelSubstrings('pearlo'));
+console.log(getVowelSubstrings('aviation'));
+console.log(getConsonantSubstrings('aviation'));
 console.log(getConsonantSubstrings('motor'));
